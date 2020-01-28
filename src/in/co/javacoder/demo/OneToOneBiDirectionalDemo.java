d7fbf005f83c7ca230ad478047363462b4e24459
Mohamed Ismail Arif <mhdarif.vdm@gmail.com>
2020-01-28 21:44:21 +0530

    cascade operation delete on one to one uni directional relationship
    create & delete one to one bi-directional

---
+++ src/in/co/javacoder/demo/OneToOneBiDirectionalDemo.java
@@ -0,0 +1,52 @@
+package in.co.javacoder.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import in.co.javacoder.entity.Instructor;
+import in.co.javacoder.entity.InstructorDetail;
+
+public class OneToOneBiDirectionalDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// Retreiving the data from DB and getting an object
+			int theId = 2;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId); 
+			System.out.println("Found instructor detail: " + tempInstructorDetail);
+			
+			if(tempInstructorDetail != null) {
+							
+				System.out.println("Associated Instructor : " + tempInstructorDetail.getInstructor());
+				
+			}
+			
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+			if(session != null) session.close();
+			if(factory != null) factory.close();
+		}
+	}
+
+}
