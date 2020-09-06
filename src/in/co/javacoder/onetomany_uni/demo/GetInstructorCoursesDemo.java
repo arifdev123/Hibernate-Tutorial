e4c732640c3df102f8bb564507c473708269f172
mhd_a <mhd_a@LAPTOP-0MTQMD3A>
2020-09-06 09:51:03 +0530

    one to many - uni directional : imports organised

--- src/in/co/javacoder/onetomany_uni/demo/GetInstructorCoursesDemo.java
+++ src/in/co/javacoder/onetomany_uni/demo/GetInstructorCoursesDemo.java
@@ -4,9 +4,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import in.co.javacoder.onetomany.entity.Course;
-import in.co.javacoder.onetomany.entity.Instructor;
-import in.co.javacoder.onetomany.entity.InstructorDetail;
+import in.co.javacoder.onetomany_uni.entity.Course;
+import in.co.javacoder.onetomany_uni.entity.Instructor;
+import in.co.javacoder.onetomany_uni.entity.InstructorDetail;
+
 
 public class GetInstructorCoursesDemo {
 
